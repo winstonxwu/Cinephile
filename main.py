eb39c548a85d8a72f91c1a9c133706f0fbcdcbd1
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-06-28 22:02:24 -0400

    movie data cleanup; line and conversation loading

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
-import torch
-import torchvision
+import os
+import re
+
+DATA_PATH = '/Users/winstonxwu/Downloads/archive'
+LINES_FILE = os.path.join(DATA_PATH, 'movie_lines.txt')
+CONVERSATIONS_FILE = os.path.join(DATA_PATH, 'movie_conversations.txt')
+
+def load_lines():
+    lines = {}
+    with open(LINES_FILE, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            parts = line.split(' +++$+++ ')
+            if len(parts) == 5:
+                line_id = parts[0]
+                text = parts[4].strip()
+                lines[line_id] = text
+    return lines
+
+def load_conversation():
+    conversations = []
+    with open(CONVERSATIONS_FILE, 'r', encoding='iso-8859-1') as f:
+        conversations = []
+        for line in f:
+            parts = line.split(' +++$+++ ')
+            if len(parts) == 4:
+                line_ids_str = parts[3].strip()
+                try:
+                    line_ids = eval(line_ids_str)
+                    conversations.append(line_ids)
+                except SyntaxError:
+                    print(f"Skipping malformed line ID String: {line_ids_str}")
+        return conversations
+    
+# def extract_qa_pairs(lines, conversations):
+    
